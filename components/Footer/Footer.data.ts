e0957f6211f1486148257b624b5abab29955f2e4
RogerCiv <rogeliocivantos@gmail.com>
2024-07-03 20:28:25 +0200

    feat: Add social media links to Footer component

---
+++ components/Footer/Footer.data.ts
@@ -0,0 +1,25 @@
+import { SiGithub, SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si";
+import { SocialType } from "./Footer.type";
+
+export const dataSocial: SocialType[] = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/',
+    icon: SiTwitter,
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/',
+    icon: SiInstagram,
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/',
+    icon: SiLinkedin,
+  },
+  {
+    name: 'GitHub',
+    url: 'https://www.github.com/RogerCiv/',
+    icon: SiGithub,
+  },
+];
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import { dataSocial } from "./Footer.data";
 
-
-export  function Footer() {
+export function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="flex justify-around p-4 ">
+      <div>
+        <Link href="/">
+          <h1 className="text-2xl font-semibold">
+            Roger<span className="text-accent">Civ</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="flex gap-4">
+        {dataSocial.map((item, index) => {
+          const Icon = item.icon;
+          return (
+            <Link href={item.url} key={index} target="_blank" rel="noopener noreferrer">
+              <Icon className="text-2xl hover:opacity-75 hover:text-accent-hover" />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
