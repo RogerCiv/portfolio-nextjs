090c8367c611b8e8037338a1b751ebb1f78a9fc7
RogerCiv <rogeliocivantos@gmail.com>
2024-07-03 13:29:19 +0200

    feat: Update page layout and add Skills component

---
+++ components/Skills/Skills.data.ts
@@ -0,0 +1,40 @@
+import { SiGit, SiJavascript, SiLaravel, SiMysql, SiNextdotjs, SiPhp, SiReact, SiSymfony, SiTailwindcss } from "react-icons/si";
+
+export const skills = [
+  {
+    text: "React",
+    icon: SiReact,
+  },
+  {
+    text: "Next.js",
+    icon: SiNextdotjs,
+  },
+  {
+    text: "Tailwind",
+    icon: SiTailwindcss,
+  },
+  {
+    text: "JavaScript",
+    icon: SiJavascript,
+  },
+  {
+    text: "PHP",
+    icon: SiPhp,
+  },
+  {
+    text: "Laravel",
+    icon: SiLaravel,
+  },
+  {
+    text: "Symfony",
+    icon: SiSymfony,
+  },
+  {
+    text: "MySQL",
+    icon: SiMysql,
+  },
+  {
+    text: "Git",
+    icon: SiGit,
+  }
+]
--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,9 +1,17 @@
+"use client";
 import { Title } from "../Title";
+import { HoverEffect } from "../ui/card-hover-effect";
+import { skills } from "./Skills.data";
+
+
 
 export  function Skills() {
+
   return (
-    <div>
+    <div className="max-w-5xl mx-auto px-8">
       <Title text="Skills 🔪" className="flex flex-col items-center justify-center" />
+
+      <HoverEffect items={skills}/>
     </div>
   )
 }
