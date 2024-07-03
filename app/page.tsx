e89e0198cf5a51ac0c422f50a7cb66eb05ac5849
RogerCiv <rogeliocivantos@gmail.com>
2024-07-03 12:46:19 +0200

    feat: Add Skills component and update Title component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { Skills } from "@/components/Skills/Skills";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black overflow-hidden">
       <div className=" dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative" >
-        <div className="max-w-7xl mx-auto p-5 h-[50rem]">
+        <div className="max-w-7xl mx-auto p-5">
           <Header />
           <HeroSection />
         </div>
-        <div className="h-10 xl:h-24 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full "></div>
+        <div className="h-10 xl:h-34 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full "></div>
+      </div>
+      <div className="max-w-7xl mx-auto p-5">
+        <Skills />
       </div>
     </main>
   );
