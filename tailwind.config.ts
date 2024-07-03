9d10bee21b124f8a1854400154b0362c8a4090e4
RogerCiv <rogeliocivantos@gmail.com>
2024-07-03 12:45:20 +0200

    feat: Add new background style "bg-dot-thick"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,6 +76,11 @@ const config = {
             `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
           )}")`,
         }),
+        "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
       },
       { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
     );
@@ -87,7 +92,7 @@ function addVariablesForColors({ addBase, theme }: any) {
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
-
+ 
   addBase({
     ":root": newVars,
   });
