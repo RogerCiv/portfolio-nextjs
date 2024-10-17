43ecca9139928e35ca3db164c4cc90208d76f585
RogerCiv <rogeliocivantos@gmail.com>
2024-10-17 12:20:53 +0200

    refactor: Update Works data with new projects
    - Add new projects to the Works data file
    - Update project descriptions and links
    - Include new technologies used in the projects

--- components/Works/Works.data.ts
+++ components/Works/Works.data.ts
@@ -1,37 +1,45 @@
-import {SiNextdotjs, SiPrisma, SiReact, SiStripe, SiSupabase, SiTailwindcss, SiThreedotjs } from "react-icons/si";
+import { SiAstro, SiNextdotjs, SiPrisma, SiReact, SiStripe, SiSupabase, SiTailwindcss, SiThreedotjs } from "react-icons/si";
 
 
- export const dataWorks = [
-    {
-      title: "Dualiza",
-      description: "This is a project 1 description",
-      tech: [SiReact, SiTailwindcss, SiThreedotjs],
-      link: "https://dualiza.ieshlanz.es",
-      cover: "/projects/dualiza.png",
-      backgroundColor: "border border-accent hover:border-indigo-500",
-    },
-    {
-      title: "SaaS Blog",
-      description: "This is a project 2 description",
-      tech: [SiReact, SiTailwindcss, SiPrisma,SiStripe, SiSupabase],
-      link: "https://saas-blog-nextjs.vercel.app",
-      cover: "/projects/saasblog.webp",
-      backgroundColor: "border border-accent hover:border-indigo-500",
-    },
-    {
-      title: "Coming Soon",
-      description: "This is a project 3 description",
-      tech: [SiReact, SiTailwindcss, SiNextdotjs],
-      link: "https://github.com/RogerCiv",
-      cover: "/projects/portfolio.webp",
-      backgroundColor: "border border-accent hover:border-indigo-500",
-    },
-    {
-      title: "Coming Soon 2",
-      description: "This is a project 4 description",
-      tech: [SiReact, SiTailwindcss, SiNextdotjs],
-      link: "https://github.com/RogerCiv",
-      cover: "/projects/dualiza.png",
-      backgroundColor: "border border-accent hover:border-indigo-500",
-    },
-  ];
+export const dataWorks = [
+  {
+    title: "Diversia Interiorismo",
+    description: "Proyecto de página web para un estudio de interiorismo.",
+    tech: [ SiTailwindcss, SiNextdotjs, SiPrisma],
+    link: "https://github.com/RogerCiv",
+    cover: "/projects/diversia.webp",
+    backgroundColor: "border border-accent hover:border-indigo-500",
+  },
+  {
+    title: "Dualiza",
+    description: "This is a project 1 description",
+    tech: [SiReact, SiTailwindcss, SiThreedotjs],
+    link: "https://dualiza.ieshlanz.es",
+    cover: "/projects/dualiza.png",
+    backgroundColor: "border border-accent hover:border-indigo-500",
+  },
+  {
+    title: "SaaS Blog",
+    description: "This is a project 2 description",
+    tech: [SiReact, SiTailwindcss, SiPrisma, SiStripe, SiSupabase],
+    link: "https://saas-blog-nextjs.vercel.app",
+    cover: "/projects/saasblog.webp",
+    backgroundColor: "border border-accent hover:border-indigo-500",
+  },
+  {
+    title: "Rental Cars",
+    description: "Proyecto de página web para una empresa de alquiler de coches.",
+    tech: [SiNextdotjs, SiTailwindcss, SiPrisma, SiStripe],
+    link: "https://github.com/RogerCiv",
+    cover: "/projects/rental-cars-test.vercel.app.webp",
+    backgroundColor: "border border-accent hover:border-indigo-500",
+  },
+  {
+    title: "Portfolio ASTRO",
+    description: "Portfolio personal con tecnología ASTRO.",
+    tech: [SiAstro, SiTailwindcss],
+    link: "https://github.com/RogerCiv",
+    cover: "/projects/rogercivdev.vercel.webp",
+    backgroundColor: "border border-accent hover:border-indigo-500",
+  },
+];
