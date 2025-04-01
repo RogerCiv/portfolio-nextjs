8707f22c052597671d9435b75343f5a1c9fea440
RogerCiv <rogeliocivantos@gmail.com>
2025-04-01 13:00:40 +0200

    fix: Remove extra whitespace from cover image path in Works data

--- components/Works/Works.data.ts
+++ components/Works/Works.data.ts
@@ -23,7 +23,7 @@ export const dataWorks = [
     description: "This is a project 3 description",
     tech: [SiReact, SiTailwindcss, SiSupabase, SiDaisyui, SiReactrouter],
     link: "https://noken-vocabulary.vercel.app",
-    cover: "/projects/noken_vocabulary.png  ",
+    cover: "/projects/noken_vocabulary.png",
     backgroundColor: "border border-accent hover:border-indigo-500",
   },
   {
